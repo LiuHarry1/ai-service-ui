cdb82d4ea18eb9d363cf44986a781ea70fafb43c
“harry” <593954214@qq.com>
2023-06-22 17:18:15 +0800

    code change

--- src/app/sentiment-analysis/sentiment-analysis.component.ts
+++ src/app/sentiment-analysis/sentiment-analysis.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sentiment-analysis',
@@ -9,11 +10,26 @@ export class SentimentAnalysisComponent {
   textInput: string = "";
   sentimentResult: string = "";
 
+  constructor(private http: HttpClient) {}
+
   analyze() {
     // Implement the sentiment analysis logic here
     // You can use a third-party sentiment analysis library or create your own algorithm
 
+    this.http.get('http://localhost:2020/sentiment_analysis?text='+this.textInput, { responseType: 'text'}).subscribe(
+      (response) => {
+        if (typeof response === "string") {
+          this.sentimentResult = response;
+        }
+      },
+      (error) => {
+        console.error('获取数据时出错：', error);
+      }
+
+    );
+
     // For this example, assume we have obtained the sentiment result
-    this.sentimentResult = 'Positive';
+    // this.sentimentResult = 'Positive';
+    // this.sentimentResult = result;
   }
 }
