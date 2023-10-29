a3cf5c1ac5116a637b096268017f028865e3570e
茫茫人海一粒沙 <593954214@qq.com>
2023-10-29 15:45:17 +0800

    code change

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -85,6 +85,7 @@ export class ChatComponent implements AfterViewChecked, OnInit {
     this.chatMessages.push({ text: this.userMessage, type: 'user' });
 
     this.http.post<any>(host+'/api/chat', { user_message: this.userMessage }).subscribe(response => {
+
       this.chatMessages.push({ text: response.bot_response, type: 'bot' });
       this.userMessage = '';
     });
