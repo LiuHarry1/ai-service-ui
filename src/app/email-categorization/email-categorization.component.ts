f7700dd9ae6d758bc4ec4291995a49fe3034b4d5
茫茫人海一粒沙 <593954214@qq.com>
2023-12-06 19:03:20 +0800

    Update email-categorization.component.ts

--- src/app/email-categorization/email-categorization.component.ts
+++ src/app/email-categorization/email-categorization.component.ts
@@ -2,6 +2,8 @@
 
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { host } from '../app-config';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-email-categorization',
@@ -14,27 +16,47 @@ export class EmailCategorizationComponent {
   content = '';
   emailPriority: string | null = null;
 
-  constructor(private http: HttpClient) {}
+  isLoading: boolean = false;
+
+  modelName: string = 'rfc'
+  modelOptions: any[] = [
+    { label: 'Random Forest Classifier', value: 'rfc' },
+    { label: 'Deep Ml ', value: 'deep_model' },
+    ]
+
+  constructor(private http: HttpClient , private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.modelName = params['model'];
+    });
+
+    console.info("modelname:"+this.modelName)
+
+  }
 
   categorizeEmail(): void {
 
     const data = {
       sender: this.sender,
       subject: this.subject,
-      content: this.content
+      content: this.content,
+      model: this.modelName
     };
 
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' }); // Set JSON content type headers
-
     console.info(data)
+    this.isLoading = true;
     this.http
-        .post('http://localhost:2020/categorize-email', data, { headers })
+      .post(host+'/categorize-email', data, { headers })
       .subscribe(
         (response: any) => {
           this.emailPriority = response.priority;
+          this.isLoading = false;
         },
         (error) => {
           console.error('Error categorizing email:', error);
+          this.isLoading = false;
         }
       );
   }
