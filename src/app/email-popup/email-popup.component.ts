2523c10b965b5d816c6b2c771b080c682378e241
茫茫人海一粒沙 <593954214@qq.com>
2023-12-31 23:24:14 +0800

    code change

--- src/app/email-popup/email-popup.component.ts
+++ src/app/email-popup/email-popup.component.ts
@@ -1,7 +1,6 @@
-// email-popup.component.ts
-
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-email-popup',
@@ -9,10 +8,21 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./email-popup.component.css']
 })
 export class EmailPopupComponent {
+  sanitizedContent: SafeHtml;
+
   constructor(
     public dialogRef: MatDialogRef<EmailPopupComponent>,
-    @Inject(MAT_DIALOG_DATA) public email: any
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public email: any,
+    private sanitizer: DomSanitizer
+  ) {
+    console.info("sanitizedContent:"+ email['whole_email_chain'])
+    this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(email['whole_email_chain']);
+  }
+
+  getSafeContent(): SafeHtml {
+
+    return this.sanitizedContent;
+  }
 
   closePopup(): void {
     this.dialogRef.close();
--- src/app/ps-issue-email-searcher/ps-issue-email-searcher.component.ts
+++ src/app/ps-issue-email-searcher/ps-issue-email-searcher.component.ts
@@ -17,22 +17,46 @@ export class PsIssueEmailSearcherComponent {
   searchResults: any[] = [];
   modelName: string = ''
   selectedEmail: any;
-
+  inputData: any = {};
   constructor(private http: HttpClient, private sanitizer: DomSanitizer,  private route: ActivatedRoute, public dialog: MatDialog) {
 
   }
 
 
   openEmailPopup(email: any): void {
-    const dialogRef = this.dialog.open(EmailPopupComponent, {
-      width: '600px',
-      data: email
-    });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The email popup was closed');
-      // Add logic here if needed after closing the popup
-    });
+    // this.http.get<any[]>(host+`/get_email?EmailId=${email['pk_email_id']}`).subscribe((data) => {
+    //   console.info("invoking search method", data)
+    //   email['whole_email_chain']= data.wholeEmailchain;
+    //   console.info("invoking search method", email)
+    // });
+
+    this.inputData['pk_email_id'] = `${email['pk_email_id']}`
+    console.info("inputdata", this.inputData)
+    this.http.post<any>(host+`/get_email`, this.inputData)
+      .subscribe(data => {
+        console.info("invoking search method", data)
+        email['whole_email_chain']= data.wholeEmailchain;
+        console.info("invoking search method", email)
+        // Handle the prediction result as needed
+
+        const dialogRef = this.dialog.open(EmailPopupComponent, {
+          width: '600px',
+          data: email
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+          console.log('The email popup was closed');
+          // Add logic here if needed after closing the popup
+        });
+
+      }, error => {
+        console.error('Error occurred:', error);
+      });
+
+
+
+
   }
 
 
