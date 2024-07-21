306667a4de21fdc7306c6427d7291a5b1c18f2f8
茫茫人海一粒沙 <593954214@qq.com>
2024-07-21 17:15:24 +0800

    exception solver

--- src/app/exception-solver/exception-solver.component.ts
+++ src/app/exception-solver/exception-solver.component.ts
@@ -12,11 +12,12 @@ import {prompt_engineering_host} from "../app-config";
 export class ExceptionSolverComponent {
   exceptionForm: FormGroup;
   result: any ;
+  isLoading: boolean = false;
 
   constructor(private http: HttpClient, private fb: FormBuilder) {
     this.exceptionForm = this.fb.group({
       exceptionStack: [''],
-      problemDescription: ['',Validators.required],
+      problemDescription: [''],
       environmentInformation: [''],
       codeSnippets: this.fb.array([this.createCodeSnippet()])
     });
@@ -29,8 +30,8 @@ export class ExceptionSolverComponent {
 
   createCodeSnippet(): FormGroup {
     return this.fb.group({
-      description: ['', Validators.required],
-      code: ['', Validators.required]
+      description: [''],
+      code: ['']
     });
   }
 
@@ -43,12 +44,53 @@ export class ExceptionSolverComponent {
   }
 
   onSubmit() {
+    if (this.exceptionForm.invalid) {
+      this.checkValidationErrors(this.exceptionForm);
+      return;
+    }
     if (this.exceptionForm.valid) {
+      this.isLoading=true
       this.http.post<any>(prompt_engineering_host+'/fix-exception', this.exceptionForm.value)
         .subscribe(response => {
           console.info("response:"+response)
           this.result = response.data;
-        });
+          this.isLoading = false
+        }, error => {
+          this.isLoading = false
+          console.error('Error in calling the endpint of /fix-exception:', error);
+      });
+    }
+  }
+
+
+  checkValidationErrors(group: FormGroup | FormArray): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const control = group.get(key);
+      if (control instanceof FormGroup || control instanceof FormArray) {
+        this.checkValidationErrors(control);
+      } else { // @ts-ignore
+        if (control.invalid) {
+                const field = this.getFieldLabel(key);
+                alert(`${field} is required`);
+              }
+      }
+    });
+  }
+
+  getFieldLabel(field: string): string {
+    switch (field) {
+      case 'problemDescription':
+        return 'Problem Description';
+      case 'environmentInformation':
+        return 'Environment Information';
+      case 'exceptionStack':
+        return 'Exception Stack';
+      case 'description':
+        return 'Description';
+      case 'code':
+        return 'Code';
+      default:
+        return field;
     }
   }
 
